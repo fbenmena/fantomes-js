0abc30557220dbc4b284d48c1a5d1a1618332994
Farès BEN MENA <fares.benmena@gmail.com>
2022-03-17 13:16:53 +0100

    Second commit
    Le déplacement du pourfendeur de fantômes se fait par l'intermédiaire du clavier
    (flèches directionnelles).

--- fichier_04.js
+++ fichier_04.js
@@ -4,19 +4,20 @@ let colonnes = 10; // Variable globale
 let case_ligne = lignes;  // Variable globale
 let case_colonne = 1;     // Variable globale
 
+let affichage_actif = false; // Variable globale
+
 function creationTable() {
     lignes = document.getElementById("lignes").value;
     colonnes = document.getElementById("colonnes").value;
 
     if (lignes == "" || lignes == null)
 	lignes = 10;
-    if (colonnes== "" || colonnes== null)
+    if (colonnes== "" || colonnes == null)
 	colonnes = 10;
 
     case_ligne = lignes;
     case_colonne = 1;
     affichageTable();
-    creationBoutons();
 }
 
 function affichageTable() {
@@ -38,16 +39,24 @@ function affichageTable() {
     }
     output = output + "</table>";
     document.getElementById("resultat").innerHTML = output;
+    affichage_actif = true;
 }
 
-function creationBoutons() {
-    let output = "";
-    output = output + '<p><input type="button" value="haut"   onclick="versLeHaut()"/></p>';
-    output = output + '<p><input type="button" value="gauche" onclick="versLaGauche()"/></p>';
-    output = output + '<p><input type="button" value="droite" onclick="versLaDroite()"/></p>';
-    output = output + '<p><input type="button" value="bas"    onclick="versLeBas()"/></p>';
-    document.getElementById("boutons").innerHTML = output;
-}
+document.addEventListener('keydown', (e) => {
+    if (affichage_actif == true) {
+	if (e.key == "ArrowUp") {
+	    versLeHaut();
+	} else if (e.key == "ArrowDown") {
+	    versLeBas();
+	} else if (e.key == "ArrowLeft") {
+	    versLaGauche();
+	} else if (e.key == "ArrowRight") {
+	    versLaDroite();
+	} else {
+	    console.log(`property value is "${e.key}"`);
+	}
+    }
+})
 
 function versLeHaut() {
     if (case_ligne > 1) {
